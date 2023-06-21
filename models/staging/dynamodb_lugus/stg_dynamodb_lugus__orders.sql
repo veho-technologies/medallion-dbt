6be7a480d4c6d6295d56cd9faa8cd682032370f0
JP Mugizi <mugizijc@miamioh.edu>
2023-06-21 14:34:16 -0400

    explicitly cast all columns

--- models/staging/dynamodb_lugus/stg_dynamodb_lugus__orders.sql
+++ models/staging/dynamodb_lugus/stg_dynamodb_lugus__orders.sql
@@ -1,47 +1,53 @@
+WITH SOURCE AS (
+ select * from {{ source('dynamodb', 'lugus')}}
+ WHERE  1=1 AND _type = 'Order'
+
+ {{- incremental_fivetran_synced() -}}
+ {{- not_fivetran_deleted() -}}
+
+)
+
 SELECT
 -- unique ID
-  order_id,
-  updated_at
-/*
--- other IDs
-  facility_id::varchar,
-  client_id::varchar,
-  consumer_id::varchar,
-  external_id::varchar,
-  submitted_by_id::varchar,
-  address_id::varchar,
-  legacy_consumer_id::varchar,
-  legacy_facility_id::varchar,
+  CAST(order_id AS VARCHAR) AS order_id,
+-- Other IDs
+  CAST(facility_id AS VARCHAR) AS facility_id,
+  CAST(client_id AS VARCHAR) AS client_id,
+  CAST(consumer_id AS VARCHAR) AS consumer_id,
+  CAST(external_id AS VARCHAR) AS external_id,
+  CAST(submitted_by_id AS VARCHAR) AS submitted_by_id,
+  CAST(address_id AS VARCHAR) AS address_id,
+  CAST(legacy_consumer_id AS VARCHAR) AS legacy_consumer_id,
+  CAST(legacy_facility_id AS VARCHAR) AS legacy_facility_id,
 
 -- Dates and timestamps
-  updated_at::timestamptz,
-  db_updated_at::timestamp,
-  created_at::timestamptz,
-  db_created_at::timestamp,
-  consumer_expected_service_date::timestamptz,
-  sla_service_date::timestamptz,
-  sla_delivery_date::date,
-  time_zone::varchar,
+  CAST(time_zone AS VARCHAR) AS time_zone,--::varchar,
+  updated_at AT TIME ZONE 'UTC' AS updated_at,--::timestamptz,
+  CAST(db_updated_at AS TIMESTAMP(6)) AS db_updated_at,--::timestamp,
+  created_at AT TIME ZONE 'UTC' AS created_at,--::timestamptz,
+  CAST(db_created_at AS TIMESTAMP(6)) AS db_created_at,--::timestamp,
+  consumer_expected_service_date  AT TIME ZONE 'UTC' AS consumer_expected_service_date ,--::timestamptz,
+  sla_service_date AT TIME ZONE 'UTC' AS sla_service_date,--::timestamptz,
+  CAST(sla_delivery_date AS DATE) AS sla_delivery_date,--::date,
+
 
 -- fivetran
-  _fivetran_synced::timestamp,
-  _fivetran_deleted::boolean,
+  CAST(_fivetran_synced AS TIMESTAMP(6)) AS _fivetran_synced,--::timestamp,
+  CAST(_fivetran_deleted AS BOOLEAN) AS _fivetran_deleted,--::boolean,
 
 -- Other strings
-  _type::varchar,
-  last_event::varchar,
-  updated_by::varchar,
-  service_class::varchar,
-  "description"::varchar as description,
-  controller::varchar,
-  kind::varchar,
-  instructions::varchar,
-  company::varchar,
-  service_type::varchar,
-  client_name::varchar,
-  client_display_name::varchar,
-  dynamic_link::varchar
-  */
+  CAST(_type AS VARCHAR) AS _type,--::varchar,
+  CAST(last_event AS VARCHAR) AS last_event,--::varchar,
+  CAST(updated_by AS VARCHAR) AS updated_by,--::varchar,
+  CAST(service_class AS VARCHAR) AS service_class,--::varchar,
+  CAST(description AS VARCHAR) AS description,--::varchar as description,
+  CAST(controller AS VARCHAR) AS controller,--::varchar,
+  CAST(kind AS VARCHAR) AS kind,--::varchar,
+  CAST(instructions AS VARCHAR) AS instructions,--::varchar,
+  CAST(company AS VARCHAR) AS company,--::varchar,
+  CAST(service_type AS VARCHAR) AS service_type,--::varchar,
+  CAST(client_name AS VARCHAR) AS client_name,--::varchar,
+  CAST(client_display_name AS VARCHAR) AS client_display_name,--::varchar,
+  CAST(dynamic_link AS VARCHAR) AS dynamic_link --::varchar
 
-from {{ source('dynamodb', 'lugus')}}
-WHERE   _type = 'Order' and  _fivetran_deleted = false LIMIT 1000
+FROM SOURCE
