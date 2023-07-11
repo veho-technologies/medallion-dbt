e9c915e4e38e93821484b1997e012eb6138846f4
Charlie Lor <charlie.lor@shipveho.com>
2023-07-11 12:24:54 -0500

    removed dags folder, and manually build/push image

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -2,5 +2,5 @@
 
 dbt run "$@"
 dbt test "$@"
-dbt run-operation _add_schemas_to_datashare
-dbt run-operation _trigger_dbt_job
+# dbt run-operation _add_schemas_to_datashare
+# dbt run-operation _trigger_dbt_job
